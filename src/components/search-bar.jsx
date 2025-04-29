dd49d82c4913ec6ae10ec61b8b852521b20b5460
Alex <thaihung.nguyen-2@student.uts.edu.au>
2025-04-29 21:00:34 +1000

    Add search actions

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,32 +1,95 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Autocomplete from '@mui/material/Autocomplete';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
+import { useSelector } from 'react-redux';
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const navigate = useNavigate();
+  const [carFilterOption, setCarFilterOption] = useState('none');
+  const [searchValue, setSearchValue] = useState(null);
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter' && searchQuery.trim() !== '') {
-      navigate(`/products?search=${encodeURIComponent(searchQuery.trim())}`);
-    }
+  const carList = useSelector((state) => {
+    return state.store.carList;
+  });
+
+  const transformedCarList = useMemo(() => {
+    return carList.map((car) => ({
+      label: `${car.vehicle_category} - ${car.brand} - ${car.model}`,
+      key: car.vin_id,
+    }));
+  }, [carList]);
+
+  const [searchOptions, setSearchOptions] = useState([]);
+
+  useEffect(() => {
+    setSearchOptions(transformedCarList);
+  }, [transformedCarList]);
+
+  const getDistinctOptions = (options) => {
+    const distinctOptions = options.filter(
+      (option, index, self) =>
+        index === self.findIndex((o) => o.label === option.label)
+    );
+    return distinctOptions;
   };
 
-  const [carFilterOption, setCarFilterOption] = useState('none');
-
-  const handleChange = (event) => {
-    setCarFilterOption(event.target.value);
+  const handleFilterChange = (event) => {
+    const filterValue = event.target.value;
+    setCarFilterOption(filterValue);
+    setSearchValue(null);
+    switch (filterValue) {
+      case 'none':
+        setSearchOptions(transformedCarList);
+        break;
+      case 'name':
+        const nameOptions = carList.map((car) => {
+          return {
+            label: car.vehicle_category,
+            key: car.vehicle_category,
+          };
+        });
+        const distinctNameOptions = getDistinctOptions(nameOptions);
+        setSearchOptions(distinctNameOptions);
+        break;
+      case 'type':
+        const typeOptions = carList.map((car) => {
+          return {
+            label: car.car_type,
+            key: car.car_type,
+          };
+        });
+        const distinctTypeOptions = getDistinctOptions(typeOptions);
+        setSearchOptions(distinctTypeOptions);
+        break;
+      case 'brand':
+        const brandOptions = carList.map((car) => {
+          return {
+            label: car.brand,
+            key: car.brand,
+          };
+        });
+        const distinctBrandOptions = getDistinctOptions(brandOptions);
+        setSearchOptions(distinctBrandOptions);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
     <div className="d-flex justify-content-center" style={{ width: '100%' }}>
       <Autocomplete
         disablePortal
-        options={[]}
+        value={searchValue}
+        options={searchOptions}
+        getOptionKey={(option) => option.key}
+        getOptionLabel={(option) => option.label}
+        onChange={(event, newValue) => {
+          setSearchValue(newValue);
+        }}
         sx={{ width: 500 }}
         renderInput={(params) => (
           <TextField {...params} placeholder="What are you looking for?" />
@@ -36,7 +99,7 @@ const SearchBar = () => {
         id="car-options"
         value={carFilterOption}
         sx={{ width: 100 }}
-        onChange={handleChange}
+        onChange={handleFilterChange}
         placeholder="Car Type"
         style={{
           marginLeft: '10px',
--- src/storeSlice.jsx
+++ src/storeSlice.jsx
@@ -3,6 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   carList: [],
   selectedCar: null,
+  searchOption: null,
+  searchValue: null,
 };
 
 const storeSlice = createSlice({
